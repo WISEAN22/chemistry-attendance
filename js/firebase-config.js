c57caf3cbfb05af1254f544290526d6bc0a824e7
WISEAN22 <believersunity5@gmail.com>
2026-08-02 18:00:17 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyChip0Sz39SUGNzVdiE1UE2S0US5m22WY",
+  apiKey: "AIzaSyCHipOSz39SUGNzVd6iIEIU2S0US5m22WY",
   authDomain: "attendance-t-5712d.firebaseapp.com",
   projectId: "attendance-t-5712d",
   storageBucket: "attendance-t-5712d.firebasestorage.app",
   messagingSenderId: "47190405698",
-  appId: "1:47190405698:web:a661d3a5e1cecee71c73db"
+  appId: "1:47190405698:web:a661d3a5e1cecee71c73db",
+  measurementId: "G-XGS6GZ8PRR"
 };
-
-export const LOGIN_DOMAIN = "attendance.local";
-export const ADMIN_USERNAME = "admin";
-export const ADMIN_EMAIL = "admin@attendance.local";
